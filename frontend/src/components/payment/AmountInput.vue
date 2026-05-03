68a80c4a77de26fe39e747fb8f0a651a47d8fa6f
hksfho <hksfho@outlook.hk>
2026-05-03 15:28:07 +0800

    fix amount selection

--- frontend/src/components/payment/AmountInput.vue
+++ frontend/src/components/payment/AmountInput.vue
@@ -55,7 +55,7 @@ const props = withDefaults(defineProps<{
   min?: number
   max?: number
 }>(), {
-  amounts: () => [100, 200, 500, 1000, 2000, 5000],
+  amounts: () => [50, 100, 200, 500, 1000, 5000],
   min: 0,
   max: 0,
 })
