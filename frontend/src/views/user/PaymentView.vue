514dd33d2131f377d8c2e1c24d76acf2993dfe1d
hksfho <hksfho@outlook.hk>
2026-05-03 18:36:58 +0800

    Revert "fix amount selection"
    This reverts commit b9a5b09d826f7ae80b519419153ec88f1ae4f34b.

--- frontend/src/views/user/PaymentView.vue
+++ frontend/src/views/user/PaymentView.vue
@@ -43,7 +43,7 @@
             <div class="card p-6">
               <AmountInput
                 v-model="amount"
-                :amounts="[100, 200, 500, 1000, 2000, 5000]"
+                :amounts="[10, 20, 50, 100, 200, 500, 1000, 2000, 5000]"
                 :min="globalMinAmount"
                 :max="globalMaxAmount"
               />
